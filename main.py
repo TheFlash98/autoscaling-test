9ba5a5c29fd0ff6ab49f5bd98060b83e1661c54a
Sarthak Khandelwal <mastermind.sarthak@gmail.com>
2025-09-18 11:10:35 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,76 @@
+from typing import Annotated, Union
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+import os
+
+
+class Hero(SQLModel, table=True):
+    id: Union[int, None] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    age: Union[int, None] = Field(default=None, index=True)
+
+
+mysql_user = os.getenv("MYSQL_USER", "appuser")
+mysql_password = os.getenv("MYSQL_PASSWORD", "P4ssw0rd")
+mysql_host = os.getenv("MYSQL_HOST", "db")
+mysql_port = os.getenv("MYSQL_PORT", "3306")
+mysql_database = os.getenv("MYSQL_DATABASE", "fastapi_app")
+mysql_url = f"mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}:{mysql_port}/{mysql_database}"
+
+engine = create_engine(mysql_url)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.post("/heroes/")
+def create_hero(hero: Hero, session: SessionDep) -> Hero:
+    session.add(hero)
+    session.commit()
+    session.refresh(hero)
+    return hero
+
+
+@app.get("/heroes/")
+def read_heroes(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Hero]:
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
+
+
+@app.get("/heroes/{hero_id}")
+def read_hero(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
+
+
+@app.delete("/heroes/{hero_id}")
+def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    session.delete(hero)
+    session.commit()
+    return {"ok": True}
